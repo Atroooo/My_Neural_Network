d981d746913c6fb6c815503854db8216c371cb01
Atroooo <lucas.compiegne1@gmail.com>
2024-08-05 18:47:56 +0200

    backpropagation not complete, put back some comments and improve docstring

--- Neural_network/neural_network.py
+++ Neural_network/neural_network.py
@@ -8,8 +8,8 @@ class LayerDense:
         """Initializes the layer with random weights and biases.
 
         Args:
-            n_inputs (int): number of inputs
-            n_neurons (int): number of neurons
+            n_inputs (int): Number of inputs to the layer.
+            n_neurons (int): Number of neurons in the layer.
         """
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
@@ -18,9 +18,24 @@ class LayerDense:
         """Calculates the outputs of the layer.
 
         Args:
-            inputs (np.array): inputs to the layer
+            inputs (np.array): Inputs to the layer.
         """
+        self.inputs = inputs
         self.output = np.dot(inputs, self.weights) + self.biases
+
+    def backward(self, dvalues):
+        """Backpropagates the gradient of the loss function.
+
+        Args:
+            dvalues (np.array): Gradient of the loss function with respect to
+                the layer's outputs.
+        """
+        # Gradients on parameters
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+
+        # Gradient on values
+        self.dinputs = np.dot(dvalues, self.weights.T)
 
 
 class ActivationReLU:
@@ -30,9 +45,22 @@ class ActivationReLU:
         """Calculates the output of the ReLU activation function.
 
         Args:
-            inputs (np.array): inputs to the activation function
+            inputs (np.array): Inputs to the activation function.
         """
+        self.inputs = inputs
         self.output = np.maximum(0, inputs)
+
+    def backward(self, dvalues):
+        """Backpropagates the gradient of the loss function.
+
+        Args:
+            dvalues (np.array): Gradient of the loss function with respect to
+                the activation's output.
+        """
+        self.dinputs = dvalues.copy()
+
+        # Zero gradient where input values were negative
+        self.dinputs[self.inputs <= 0] = 0
 
 
 class ActivationSoftmax:
@@ -42,11 +70,37 @@ class ActivationSoftmax:
         """Calculates the output of the softmax activation function.
 
         Args:
-            inputs (np.array): inputs to the activation function
+            inputs (np.array): Inputs to the activation function.
         """
+        # Get unnormalized probabilities
         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        # Normalize them for each sample
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
+
+    def backward(self, dvalues):
+        """Calculates the gradient of the softmax activation function.
+
+        Args:
+            dvalues (np.array): Gradient of the loss function with respect to
+                the activation's output.
+        """
+        self.dinputs = np.empty_like(dvalues)
+
+        # Enumerate outputs and gradients
+        for index, (single_output, single_dvalues) in \
+                enumerate(zip(self.output, dvalues)):
+            # Flatten output array
+            single_output = single_output.reshape(-1, 1)
+
+            # Calculate Jacobian matrix of the output
+            jacobian_matrix = np.diagflat(single_output) - \
+                np.dot(single_output, single_output.T)
+
+            # Calculate sample-wise gradient
+            # and add it to the array of sample gradients
+            self.dinputs[index] = np.dot(jacobian_matrix,
+                                         single_dvalues)
 
 
 class Loss:
@@ -56,8 +110,8 @@ class Loss:
         """Calculates the loss between the predicted and true values.
 
         Args:
-            output (np.array): predicted values
-            y (np.array): true values
+            output (np.array): Predicted values.
+            y (np.array): True values.
         """
         sample_losses = self.forward(output, y)
         data_loss = np.mean(sample_losses)
@@ -68,35 +122,50 @@ class LossCategoricalCrossentropy(Loss):
     """Class to represent the categorical crossentropy loss function."""
 
     def forward(self, y_pred, y_true):
-        """Calculates the loss between the predicted and true values.
+        """Calculates the categorical crossentropy loss between the predicted
+            and true values.
 
         Args:
-            y_pred (np.array): predicted values
-            y_true (np.array): true values
+            y_pred (np.array): Predicted values.
+            y_true (np.array): True values.
         """
         samples = len(y_pred)
+
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
         y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
 
+        # Probabilities for target values -
+        # only if categorical labels
         if len(y_true.shape) == 1:
             correct_confidences = y_pred_clipped[range(samples), y_true]
 
+        # Mask values - only for one-hot encoded labels
         elif len(y_true.shape) == 2:
             correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
 
+        # Losses
         negative_log_likelihoods = -np.log(correct_confidences)
         return negative_log_likelihoods
 
     def backward(self, dvalues, y_true):
-        """Calculates the gradient of the loss function.
+        """Calculates the gradient of the categorical crossentropy
+            loss function.
 
         Args:
-            dvalues (np.array): array of predictions
-            y_true (np.array): array of true values
+            dvalues (np.array): Gradient of the loss function with respect to
+                the predictions.
+            y_true (np.array): True values.
         """
         samples = len(dvalues)
         labels = len(dvalues[0])
 
+        # If labels are sparse, turn them into one-hot vector
         if len(y_true.shape) == 1:
             y_true = np.eye(labels)[y_true]
+
+        # Calculate gradient
         self.dinputs = -y_true / dvalues
+
+        # Normalize gradient
         self.dinputs = self.dinputs / samples
